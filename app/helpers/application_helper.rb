5716461b879880a6a21613a05ee30387b46f8a10
Kevin Lam <i.am.kev@gmail.com>
2010-06-17 02:05:18 -0400

    Basic Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Kevin.cv.lam"
+    base_title = "kevincvlam"
     if @title.nil?
       base_title
     else
