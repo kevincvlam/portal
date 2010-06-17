54f8ec23f74ba9898802ad2b6e44d03adca81557
Kevin Lam <i.am.kev@gmail.com>
2010-06-17 12:58:45 -0400

    Final Layout Touches

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-map.root :controller => 'pages', :action => 'home'
-  
+map.root :controller => 'pages', :action => 'home' 
+map.about   '/about',   :controller => 'pages', :action => 'about'
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
