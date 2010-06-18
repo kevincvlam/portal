a228953c4f7de9ce5cf39c77ce4f619b28a35dda
Kevin Lam <i.am.kev@gmail.com>
2010-06-17 23:36:53 -0400

    Revert "Twitter?"
    This reverts commit 59dcfc104a05dbbdb6bc42478c129f45ad2ed376.

--- app/controllers/timeline_controller.rb~
+++
@@ -1,2 +0,0 @@
-class TimelineController < ApplicationController
-end
--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,6 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  config.gem "twitter-auth", :lib => 'twitter_auth'
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
